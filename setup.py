37b1bfc38ee4953c014c2437737c4a5594548419
Bryan Bishop <kanzure@gmail.com>
2013-08-04 22:52:30 -0500

    bump version to v0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="requestions",
-    version="0.0.4",
+    version="0.0.5",
     url="https://github.com/kanzure/python-requestions",
     license="BSD",
     author="Bryan Bishop",
